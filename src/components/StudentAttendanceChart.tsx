06dc43cbf5b88d34496ad7883dd208c88ab3a298
mustofa adny <adniadni04@gmail.com>
2025-10-11 11:35:52 +0700

    feat: fetch student attendance

---
+++ src/components/StudentAttendanceChart.tsx
@@ -0,0 +1,25 @@
+import prisma from "@/lib/prisma";
+import React from "react";
+
+const StudentAttendanceChart = async ({ id }: { id: string }) => {
+  const attendance = await prisma.attendance.findMany({
+    where: {
+      studentId: id,
+      date: {
+        gte: new Date(new Date().getFullYear(), 0, 1),
+      },
+    },
+  });
+
+  const totalDays = attendance.length;
+  const presentDays = attendance.filter((d) => d.present).length;
+  const percentage = (presentDays / totalDays) * 100;
+  return (
+    <div className="">
+      <h1 className="text-xl font-semibold">{percentage}%</h1>
+      <span className="text-sm text-gray-400">Attendance</span>
+    </div>
+  );
+};
+
+export default StudentAttendanceChart;
