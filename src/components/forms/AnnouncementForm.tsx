fdf5e7b36cecdbc45e7ae4e284dab089049315b5
mustofa adny <adniadni04@gmail.com>
2025-10-20 10:50:12 +0700

    ui: announcement form

---
+++ src/components/forms/AnnouncementForm.tsx
@@ -0,0 +1,145 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import InputField from "../InputField";
+import {
+  AnnouncementInput,
+  announcementSchema,
+  EventInput,
+  eventSchema,
+} from "@/lib/formValidationSchemas";
+import { Dispatch, SetStateAction, useEffect } from "react";
+import { useFormState } from "react-dom";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
+import { createEvent, updateEvent } from "@/lib/actions";
+
+const AnnouncementForm = ({
+  type,
+  data,
+  setOpen,
+  relatedData,
+}: {
+  type: "create" | "update";
+  data?: any;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+  relatedData?: any;
+}) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<AnnouncementInput>({
+    resolver: zodResolver(announcementSchema),
+  });
+  //   after react 19 it'l be useactionstate
+  const [state, formAction] = useFormState(
+    type === "create" ? createEvent : updateEvent,
+    {
+      success: false,
+      error: false,
+    }
+  );
+  const router = useRouter();
+
+  const onSubmit = handleSubmit((data) => {
+    console.log("data", data);
+    formAction(data);
+  });
+
+  useEffect(() => {
+    if (state.success) {
+      toast.success(
+        `Announcement has been ${
+          type === "create" ? "created" : "updated"
+        } successfully`
+      );
+      setOpen(false);
+      router.refresh();
+    }
+  }, [state, type, router, setOpen]);
+
+  const { classes } = relatedData;
+
+  return (
+    <form
+      className="flex flex-col gap-8 overflow-y-scroll max-h-[90vh] pr-2 no-scrollbar"
+      onSubmit={onSubmit}
+    >
+      <h1 className="text-xl font-semibold">
+        {type === "create"
+          ? "Create a new announcement"
+          : "Update announcement"}
+      </h1>
+      <div className="flex justify-between flex-wrap gap-4 mx-3">
+        <InputField
+          label="Event Title"
+          name="title"
+          defaultValue={data?.title}
+          register={register}
+          error={errors?.title}
+          widthContainer="md:w-full"
+        />
+        <InputField
+          label="Event Description"
+          name="description"
+          defaultValue={data?.description}
+          register={register}
+          error={errors?.description}
+          widthContainer="md:w-full"
+          textArea
+        />
+        <InputField
+          label="Date"
+          name="date"
+          defaultValue={data?.date?.toISOString().slice(0, 16)}
+          register={register}
+          error={errors?.date}
+          widthContainer="md:w-full"
+          type="datetime-local"
+        />
+        {data && (
+          <InputField
+            label="Id"
+            name="id"
+            defaultValue={data?.id}
+            register={register}
+            error={errors?.id}
+            widthContainer="md:w-full"
+            hidden
+          />
+        )}
+
+        <div className="flex flex-col gap-2 w-full">
+          <label className="text-xs text-gray-500">Class</label>
+          <select
+            className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full"
+            {...register("classId")}
+            defaultValue={data?.classId}
+          >
+            {classes?.map((classItem: { id: string; name: string }) => (
+              <option value={classItem.id} key={classItem.id} className="p-2">
+                {classItem.name}
+              </option>
+            ))}
+          </select>
+          {errors.classId?.message && (
+            <p className="text-xs text-red-400">
+              {errors.classId.message.toString()}
+            </p>
+          )}
+        </div>
+      </div>
+
+      {state?.error && (
+        <span className="text-red-500">Something went wrong</span>
+      )}
+      <button className="bg-[#FEBA17] text-white p-2 rounded-md">
+        {type === "create" ? "Create" : "Update"}
+      </button>
+    </form>
+  );
+};
+
+export default AnnouncementForm;
