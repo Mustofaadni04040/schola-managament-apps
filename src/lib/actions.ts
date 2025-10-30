6a432835b4c0a20770f8eaf4666a616f3ed9ab71
mustofa adny <adniadni04@gmail.com>
2025-10-30 17:11:18 +0700

    feat: delete attendance using server action

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1033,3 +1033,24 @@ export const updateAttendance = async (
     return { success: false, error: true };
   }
 };
+
+export const deleteAttendance = async (
+  currentState: CurrentState,
+  data: FormData
+) => {
+  const id = data.get("id") as string;
+
+  try {
+    await prisma.attendance.delete({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    // revalidatePath("/list/subjects");
+    return { success: true, error: false };
+  } catch (error) {
+    console.log(error);
+    return { success: false, error: true };
+  }
+};
